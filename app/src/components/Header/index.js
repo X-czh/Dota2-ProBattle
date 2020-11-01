e63ec8474c24c1a3889f7a0ea31e9f1816cd5289
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-01 01:51:47 -0500

    Update index.js

--- app/src/components/Header/index.js
+++ app/src/components/Header/index.js
@@ -4,9 +4,17 @@ import './styles.scss';
 class Header extends React.Component {
   render() {
     return (
-      <div className="Header">
-        Dota2 ProBattle
-      </div>
+        <header className="page_header">
+          <nav className="navbar_menu">
+            <ul id="navbar_list">
+              <a className="menu_link"><li>Dota2 ProBattle</li></a>
+              <a className="menu_link"><li>Section1</li></a>
+              <a className="menu_link"><li>Section2</li></a>
+              <a className="menu_link"><li>Section3</li></a>
+              <a className="menu_link"><li>Section4</li></a>
+            </ul>
+          </nav>
+        </header>
     );
   }
 }
