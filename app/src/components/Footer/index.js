15404fc4fa1892d0671e9ade6c9752e62b639173
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-01 01:49:03 -0500

    Add files via upload

---
+++ app/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import './styles.scss';
+import Body from "../Body";
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer>Author:</ footer>
+        )
+    }
+}
+export default Footer;
