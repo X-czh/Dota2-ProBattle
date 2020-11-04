26b31255763f1a94d71a58c3b2006560a3c7a6fa
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-04 01:48:20 -0600

    Add files via upload

---
+++ app/src/components/ItemApp/index.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+// import PropTypes from "prop-types";
+import ItemSearch from '../ItemSearch/index'
+import ItemList from '../ItemList/index'
+import './styles.scss';
+import axios from 'axios'
+
+export default class ItemApp extends Component {
+  state = {
+    heros: '',
+    items: []
+  }
+
+  itemSearch = (allhero) => {
+    // const url = 'http://127.0.0.1:5000/addhero'
+    // axios.post(url, allhero)
+    //   .then(res => {
+    //     console.log('response: ',res)
+    //   })
+    //   .catch(err => console.log(err))
+
+  //   this.state.heros = allhero
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Recommendation of item</h2>
+        <ItemSearch itemSearch={this.itemSearch}/>
+        <ItemList />
+      </div>
+    );
+  }
+}
+
---
+++ app/src/components/ItemList/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export default class ItemList extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Recommended items:</h3>
+                <ul>
+                    <li>item</li>
+                    <li>item</li>
+                    <li>item</li>
+                    <li>item</li>
+                    <li>item</li>
+                    <li>item</li>
+                </ul>
+            </div>
+            
+        )
+    }
+}
