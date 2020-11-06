15d1039702c894cd11de770822a2f08eecaca704
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-06 17:22:30 -0600

    Update index.js

--- app/src/components/HeroOutput/index.js
+++ app/src/components/HeroOutput/index.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 export default class HeroOutput extends Component{
+
+    static propTypes = {
+        heroes: PropTypes.array.isRequired,
+    }
+
     render(){
+        const {heroes} = this.props
         return(
             <div>
-                <h3>Opponent's most used heros</h3>
+                <h3>Opponent's most used heroes</h3>
                 <ul>
-                    <li>Hero name, number od matched, win rate</li>
+                    {heroes.map((hero,index) => (<li key={index}>`Hero: ${hero.hero_name}, number of games: ${hero.num_games}, winning rate: ${hero.win_rate}`</li>))}
                 </ul>
-            </div>
+            </div> 
         )
     }
 }
