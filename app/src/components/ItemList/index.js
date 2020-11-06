4fc94035dd0a2cdfdf0d346da7c3c6ba136c1eb2
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-05 20:19:12 -0600

    Update index.js

--- app/src/components/ItemList/index.js
+++ app/src/components/ItemList/index.js
@@ -1,20 +1,22 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 
 export default class ItemList extends Component{
+    
+    static propTypes = {
+        items: PropTypes.array.isRequired,
+    }
+    
     render(){
+        const {items} = this.props
         return(
             <div>
                 <h3>Recommended items:</h3>
                 <ul>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
+                    {items.map((item,index) => (<li key={index}>{item}</li>))}
                 </ul>
             </div>
             
-        )
+        ) 
     }
 }
