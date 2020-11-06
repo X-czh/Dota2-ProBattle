e3116a4f08e21524c4e5c3dc34c7d34478acd0e1
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-06 17:22:08 -0600

    Update index.js

--- app/src/components/HeroApp/index.js
+++ app/src/components/HeroApp/index.js
@@ -4,20 +4,29 @@ import HeroOutput from '../HeroOutput'
 import axios from 'axios'
 
 export default class HeroApp extends Component{
+    state = {
+        heroes: ''
+    }
+    
     submitId = (id) => {
+        let {heroes} = this.state
         console.log(id)
         const url = 'http://127.0.0.1:5000/debuffOpponentHero'
         axios.post(url, id)
             .then(res => {
+                heroes = Object(res.data)
+                this.setState({heroes})
                 console.log(res.data)
             })
+            .catch(err => console.log(err))
     }
     render(){
+        const {heros} = this.state
         return(
             <div>
                 <h2>Opponents' signature heroes</h2>
                 <HeroInput submitId = {this.submitId}/>
-                <HeroOutput/>
+                <HeroOutput heroes = {heroes}/>
             </div>
         )
     }
