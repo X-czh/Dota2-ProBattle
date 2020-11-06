7923bbc529f5e00fc39bae0ba38682e920616c72
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-05 20:19:32 -0600

    Update index.js

--- app/src/components/ItemApp/index.js
+++ app/src/components/ItemApp/index.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-// import PropTypes from "prop-types";
 import ItemSearch from '../ItemSearch/index'
 import ItemList from '../ItemList/index'
 import './styles.scss';
@@ -12,22 +11,25 @@ export default class ItemApp extends Component {
   }
 
   itemSearch = (allhero) => {
+    let {items} = this.state
     const url = 'http://127.0.0.1:5000/debuffOpponentItem'
     axios.post(url, allhero)
       .then(res => {
-        console.log('response: ',res)
+        items = Object.values(res.data)
+        this.setState({items})
+        console.log('response: ',items)
       })
       .catch(err => console.log(err))
-
-  //   this.state.heros = allhero
   }
 
+
   render() {
+    const {items} = this.state
     return (
       <div>
         <h2>Recommendation of item</h2>
-        <ItemSearch itemSearch={this.itemSearch}/>
-        {/* <ItemList /> */}
+        <ItemSearch itemSearch = {this.itemSearch}/>
+        <ItemList items={items} />
       </div>
     );
   }
