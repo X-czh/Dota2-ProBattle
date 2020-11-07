c48174b79eef97d8f748055e80156d4b895bd03e
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-07 00:34:03 -0600

    Update index.js

--- app/src/components/ItemList/index.js
+++ app/src/components/ItemList/index.js
@@ -17,13 +17,15 @@ export default class ItemList extends Component{
                     </ul>
                 </div>
             )
-        } else if (items[0].wrong == 0) {
+        } else if (items[0] === 0) {
+            return(
                 <div>
                     <h3>Opponent's most used heroes</h3>
                     <ul>
                         <li>There might be something wrong with your input, please check the heroes you input.</li>
                     </ul>
                 </div> 
+            )
         } else {
             return(
                 <div>
