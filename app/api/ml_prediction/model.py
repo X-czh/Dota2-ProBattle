71bf09ace48caf43483c1fab78c18a147e202315
zjzijielu <zj.zijielu@outlook.com>
2020-11-23 19:34:07 -0800

    add model prediction

--- app/api/ml_prediction/model.py
+++ app/api/ml_prediction/model.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 import pickle
 
@@ -7,19 +8,24 @@ class Model:
     
     def __init__(self, path, C=0.005):
         self.path = path
+        self.num_of_heores = 119
         if path != '':
-            self.model = pickle.load(path + "/model.pkl")
-            self.scaler = pickle.load(path + "/scaler.pkl")
+            print(path + "/model.pkl")
+            with open(path + "/model.pkl", "rb") as model_pkl:
+                self.model = pickle.load(model_pkl)
+            with open(path + "/scaler.pkl", "rb") as scaler_pkl:
+                self.scaler = pickle.load(scaler_pkl)
         else:
             self.model = LogisticRegression(C=C)
             self.scaler = StandardScaler()
 
-    def train(train_data):
-        x_train, y_train = train_data 
-        scaler.fit(x_train)
-        x_train = scaler.transform(x_train)
-        model.fit(x_train, y_train)
-        
+    def train(self, x_train, y_train):
+        self.scaler.fit(x_train)
+        x_train = self.scaler.transform(x_train)
+        print("start training...")
+        self.model.fit(x_train, y_train)
+        print("finish training. saving model...")
+
         # save model and scaler
         with open(self.path + "model.pkl", "wb") as file:
             pickle.dump(self.model, file)
@@ -27,9 +33,17 @@ class Model:
         with open(self.path + "scaler.pkl", "wb") as file:
             pickle.dump(self.scaler, file)
 
-    def predict(test_data):
-        test_data = scaler.transform(test_data)
-        prob = model.predict_proba(test_data)
+    def predict(self, radiant_heroes, dire_heroes):
+        """
+            return the probability of radiant winning the game
+        """
+        features = np.zeros(2 * self.num_of_heores)
+        for i in range(5):
+            features[radiant_heroes[i] - 1] = 1
+            features[dire_heroes[i] - 1 + self.num_of_heores] = 1
+        features_reshaped = features.reshape(1, -1)
+        features_final = self.scaler.transform(features_reshaped)
+        prob = self.model.predict_proba(features_final)[:, 1]
 
         return prob
 
--- app/api/ml_prediction/train.py
+++ app/api/ml_prediction/train.py
@@ -1,5 +1,48 @@
 from model import Model
+import pandas as pd
+import numpy as np
+
+def load_dataset(csv_path):
+    df = pd.read_csv(csv_path, encoding='unicode_escape')
+
+    low_mmr = 500
+    high_mmr = 2000
+
+    df = df[df.avg_mmr > low_mmr]
+    df = df[df.avg_mmr < high_mmr]
+    
+    print("the dataset contains {} games".format(len(df)))
+
+    num_of_heroes = 119 # hardcod
+
+    # convert df to features
+    x_matrix = np.zeros((df.shape[0], 2 * num_of_heroes))
+    y_matrix = np.zeros(df.shape[0])
+
+    df_np = df.values
+
+    for i, row in enumerate(df_np):
+        radiant_win = row[1]
+        radiant_heroes = list(map(int, row[2].split(',')))
+        dire_heroes = list(map(int, row[3].split(',')))
+
+        for j in range(5):
+            x_matrix[i, radiant_heroes[j] - 1] = 1
+            x_matrix[i, dire_heroes[j] - 1 + num_of_heroes] = 1
+        
+        y_matrix[i] = 1 if radiant_win else 0
+
+    return x_matrix, y_matrix
 
 def main():
-    model = Model("./")
+    model = Model(path="")
+
+    # load data
+    csv_path = "706e_train_dataset.csv"
+    x_train, y_train = load_dataset(csv_path)
+
+    model.train(x_train, y_train)
     
+
+if __name__ == "__main__":
+    main()
