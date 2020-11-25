c44115d0f9b222023735613f342cd0398b32a731
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-25 04:03:32 -0600

    Update index.js

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -17,6 +17,7 @@ import DeleteMatch from '../DeleteMatch';
 import SearchMatch from '../SearchMatch';
 import UpdateMatch from '../UpdateMatch';
 import WinChance from '../WinChance';
+import HeroCounter from '../HeroCounter'
 
 class App extends React.Component {
   componentDidMount() {
@@ -31,116 +32,124 @@ class App extends React.Component {
   render() {
     const { activeItem } = this.state
 
-    return (
-      <div className="App">
-        <Router>
-          <MyHeader />
-          <Menu>
-            <Menu.Item 
-              as={ Link }
-              name="item"
-              active={activeItem === 'item'}
-              to="/item"
-              onClick={this.handleItemClick}>
-              Items
-            </Menu.Item>
-            <Dropdown text="Match" pointing className='link item'>
-              <Dropdown.Menu>
-                <Dropdown.Header>Search</Dropdown.Header>
+       return (
+        <div className="App">
+          <Router>
+            <MyHeader />
+            <Menu>
+              <Dropdown text="Match" pointing className='link item'>
+                <Dropdown.Menu>
+                  <Dropdown.Header>Search</Dropdown.Header>
                   <Dropdown.Item
-                    text="Search by match ID"
-                    as = { Link }
-                    name="searchMatchByMatchID"
-                    to="/searchMatchByMatchID"
+                      text="Search by match ID"
+                      as = { Link }
+                      name="searchMatchByMatchID"
+                      to="/searchMatchByMatchID"
                   />
                   <Dropdown.Item
-                    text="Search by account ID"
-                    as = { Link }
-                    name="searchMatchByAccountID"
-                    to="/searchMatchByAccountID" 
+                      text="Search by account ID"
+                      as = { Link }
+                      name="searchMatchByAccountID"
+                      to="/searchMatchByAccountID"
                   />
-                <Dropdown.Header>Add</Dropdown.Header>
+                  <Dropdown.Header>Add</Dropdown.Header>
                   <Dropdown.Item
-                    text="Add by match ID"
-                    as = { Link }
-                    name="addMatchByMatchID"
-                    to="/addMatchByMatchID"
+                      text="Add by match ID"
+                      as = { Link }
+                      name="addMatchByMatchID"
+                      to="/addMatchByMatchID"
                   />
                   <Dropdown.Item
-                    text="Add by account ID"
-                    as = { Link }
-                    name="addMatchByAccountID"
-                    to="/addMatchByAccountID" 
+                      text="Add by account ID"
+                      as = { Link }
+                      name="addMatchByAccountID"
+                      to="/addMatchByAccountID"
                   />
-                <Dropdown.Header>Update</Dropdown.Header>
-                <Dropdown.Item
-                  text="Update start time"
-                  as = { Link }
-                  name="updateMatch"
-                  to="/updateMatch"
-                />
-                <Dropdown.Header>Delete</Dropdown.Header>
-                <Dropdown.Item
-                  text="Delete match"
-                  as = { Link }
-                  name="deleteMatch"
-                  to="/deleteMatch" 
-                />
-              </Dropdown.Menu>
-            </Dropdown>
-            <Dropdown text="Battle Guide" pointing className='link item'>
-              <Dropdown.Menu>
-                <Dropdown.Header>Basic</Dropdown.Header>
-                <Dropdown.Item
-                text="Opponents' signature heroes"
-                as = { Link }
-                name="HeroApp"
-                to="/HeroApp" />
-                <Dropdown.Header>Advanced</Dropdown.Header>
-                <Dropdown.Item
-                text="Winning Chance Prediction"
-                as = { Link }
-                name="WinChance"
-                to="/WinChance" />
-              </Dropdown.Menu>
-            </Dropdown>
-           
-          </Menu>
-          {/* <Body /> */}
+                  <Dropdown.Header>Update</Dropdown.Header>
+                  <Dropdown.Item
+                      text="Update start time"
+                      as = { Link }
+                      name="updateMatch"
+                      to="/updateMatch"
+                  />
+                  <Dropdown.Header>Delete</Dropdown.Header>
+                  <Dropdown.Item
+                      text="Delete match"
+                      as = { Link }
+                      name="deleteMatch"
+                      to="/deleteMatch"
+                  />
+                </Dropdown.Menu>
+              </Dropdown>
+              <Dropdown text="Battle Guide" pointing className='link item'>
+                <Dropdown.Menu>
+                  <Dropdown.Header>Basic</Dropdown.Header>
+                  <Dropdown.Item
+                      as={ Link }
+                      name="item"
+                      active={activeItem === 'item'}
+                      to="/item"
+                      onClick={this.handleItemClick}>
+                    Items
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                      text="Opponents' signature heroes"
+                      as = { Link }
+                      name="HeroApp"
+                      to="/HeroApp" />
+                  <Dropdown.Header>Advanced</Dropdown.Header>
+                  <Dropdown.Item
+                      text="Winning Chance Prediction"
+                      as = { Link }
+                      name="WinChance"
+                      to="/WinChance" />
+                  <Dropdown.Item
+                      text="Recommendation of Hero Counters"
+                      as = { Link }
+                      name="HeroCounter"
+                      to="/HeroCounter" />
+                </Dropdown.Menu>
+              </Dropdown>
 
-          <Switch>
-            <Route path="/" component={ Body } exact />
-            <Route path="/item" >
-              <ItemApp />
-            </Route>
-            <Route path="/searchMatchByMatchID">
-              <SearchMatch searchType="MatchID" />
-            </Route>
-            <Route path="/searchMatchByAccountID">
-              <SearchMatch searchType="AccountID" />
-            </Route>
-            <Route path="/addMatchByMatchID">
-              <AddMatch addType="MatchID" />
-            </Route>
-            <Route path="/addMatchByAccountID">
-              <AddMatch addType="AccountID" />
-            </Route>
-            <Route path="/updateMatch">
-              <UpdateMatch />
-            </Route>
-            <Route path="/deleteMatch">
-              <DeleteMatch />
-            </Route>
-            <Route path="/HeroApp">
-              <HeroApp />
-            </Route>
-            <Route path="/WinChance">
-              <WinChance />
-            </Route>
-          </Switch>
-        </Router>
-        <Footer />
-      </div>
+            </Menu>
+            {/* <Body /> */}
+
+            <Switch>
+              <Route path="/" component={ Body } exact />
+              <Route path="/item" >
+                <ItemApp />
+              </Route>
+              <Route path="/searchMatchByMatchID">
+                <SearchMatch searchType="MatchID" />
+              </Route>
+              <Route path="/searchMatchByAccountID">
+                <SearchMatch searchType="AccountID" />
+              </Route>
+              <Route path="/addMatchByMatchID">
+                <AddMatch addType="MatchID" />
+              </Route>
+              <Route path="/addMatchByAccountID">
+                <AddMatch addType="AccountID" />
+              </Route>
+              <Route path="/updateMatch">
+                <UpdateMatch />
+              </Route>
+              <Route path="/deleteMatch">
+                <DeleteMatch />
+              </Route>
+              <Route path="/HeroApp">
+                <HeroApp />
+              </Route>
+              <Route path="/WinChance">
+                <WinChance />
+              </Route>
+              <Route path="/HeroCounter">
+                <HeroCounter />
+              </Route>
+            </Switch>
+          </Router>
+          <Footer />
+        </div>
     );
   }
 }
