1a692277712f3f55fc6637c5dc1aa0128a0a0290
TL021 <72060161+TL021@users.noreply.github.com>
2020-11-30 17:25:53 -0600

    Update index.js

--- app/src/components/Body/index.js
+++ app/src/components/Body/index.js
@@ -9,28 +9,24 @@ class Body extends React.Component{
                 <section id="section1" data-nav="Section 1" className="your-active-class">
                     <div className="landing__container">
                         <h2 className="nav_title">Opponents’ most commonly used heroes</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-                        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-                    </div>
-                </section>
-                <section id="section1" data-nav="Section 1" className="your-active-class">
-                    <div className="landing__container">
-                        <h2 className="nav_title">Counter picks of opponents’ heroes</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-                        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+                        <p>ProBattle scrapes the recent battle histories of the opponents and suggest which heroes to ban during pick/ban phase based on the query results.</p>
                     </div>
                 </section>
                 <section id="section1" data-nav="Section 1" className="your-active-class">
                     <div className="landing__container">
                         <h2 className="nav_title">Recommendation of items</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-                        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+                        <p>From the most recent battle histories of specific hero, ProBattle recommends items with the highest win-rate against the opponents’ team.</p>
+                    </div>
+                </section>
+                <section id="section1" data-nav="Section 1" className="your-active-class">
+                    <div className="landing__container">
+                        <h2 className="nav_title">Winning Chance Prediction</h2>
+                        <p>Our application can predict the winning chance against the opponents’ lineup using machine learning algorithms. More concretely, each hero can be mapped to a vector in a vector space using word2vec, the lineup of a team can thereby be represented with a composition of hero vectors. We can then train a machine learning model on history match data, which takes the representation vectors of the two lineups in the match as input and output the relative win percentage.</p>
                     </div>
                 </section><section id="section1" data-nav="Section 1" className="your-active-class">
                     <div className="landing__container">
-                        <h2 className="nav_title">Winning chance against the opponents’ lineup</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-                        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+                        <h2 className="nav_title">Recommendation of Hero Counters</h2>
+                        <p>Counter picks of opponents’ heroes can be conveniently stored in a graph DB, where the nodes are heroes, and the edges represent the relative win percentage between the two heroes. We can precompute the relative win percentage between each pair of heroes, and cache the results in a graph DB to accelerate the query time. The content in the graph DB can be updated periodically with recent match data.</p>
                     </div>
                 </section>
             </div>
